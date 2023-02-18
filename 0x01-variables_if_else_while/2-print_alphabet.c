08b5f5004453da4ec9a6ac3dcbc1d7cae63d800b
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-02-17 16:26:45 -0800

    f

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main -entry point
+ *Return: Always o (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 97 ; alpha > 123 ; alpha++)
+		putchar(alpha);
+	return (0);
+}
