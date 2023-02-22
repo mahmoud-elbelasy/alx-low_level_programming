1f0b5a820e2769df9075f501a985c53f80df3ed1
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-02-21 17:12:00 -0800

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - give the absolute of number
+ * @c: the number
+ * Return: Always 0 (success)
+ */
+int _abs(int c)
+{
+	int r;
+
+	r = abs(c);
+
+	return (r);
+}
