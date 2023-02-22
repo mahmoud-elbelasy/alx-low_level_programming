772b161bde4127855d0064d7ac0de6d1c6312110
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-02-21 16:35:19 -0800

    islowe

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,21 @@
 #include "main.h"
 #include <ctype.h>
+
 /**
  * _islower - function that test the lowercase
+ * @c: the charactar
  * Return: Always 0 (success)
  */
+
 int _islower(int c)
 {
 	int n;
-	n = 1;
 
-	if (islower(c) == 1)
+	if (islower(c))
 		n = 1;
 	else
 		n = 0;
 	return (n);
-}	
+}
 
 
