ab1c9bb14f56cdab4dd75a0edae99eba29b07960
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-02-28 11:13:02 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int i;
+	int counter;
+
+	counter = strlen(s);
+
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		rev = s[i];
+		s[i] = s[counter];
+		s[counter] = rev;
+	}
+
+}
+
