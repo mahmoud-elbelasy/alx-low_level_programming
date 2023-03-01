ab293b8dfd4b942f7fa332812193ab6402412f31
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-03-01 04:45:41 -0800

    cut

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strcat - concatenations two strings
+ * @dest: the first string
+ * @src: the second string
+ * @n: the number of digit that will apend from src
+ * Return: a string
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr;
+
+	ptr = strncat(dest, src, n);
+	return (ptr);
+}
