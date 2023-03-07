c1c56405f8c4c93a4083444d50a73c76e807bc5e
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-03-06 17:24:46 -0800

    easy

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
-
+void set_string(char **s, char *to);
 char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
