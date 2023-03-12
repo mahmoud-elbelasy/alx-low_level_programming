99da74ef8c036e331d838bf8ec203883eccb6908
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-03-12 11:31:56 -0700

    mul

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,7 +4,7 @@
  * main - program that prints all arguments it receives
  * @args: the nums of argument
  * @argv: array of arrgument
- * return: Always 0
+ * Return: Always 0
  */
 int main(int args, char *argv[])
 {
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ * main - program that multiplies two numbers
+ * @ars : nums of argument
+ * @argv : the argument
+ * Return: Always 0
+ */
+int main(int args, char *argv[])
+{
+	int i, mul;
+
+	if (args > 2)
+	{
+		for (i = 1; i < args; i++)
+		{
+			mul = atoi(argv[1]) * atoi(argv[2]);
+		}
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
