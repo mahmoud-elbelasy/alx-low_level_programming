040e63ce4a0360b8da1733df21174c5e4e58912b
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-03-14 16:30:59 -0700

    update

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,6 +11,14 @@ char *str_concat(char *s1, char *s2)
 	int i, r, n, s;
 	char *p;
 
+	if (s1 == NULL)
+
+		s1 = "";
+
+	if (s2 == NULL)
+
+		s2 = "";
+
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
