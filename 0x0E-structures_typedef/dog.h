1496dd777a10955aec08497b62f8ee142fd8c8cc
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-03-20 19:39:31 -0700

    good

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include <stdlib.h>
+#ifndef _HEADER_
+#define _HEADER_
 
 /**
  * struct dog - Dog attributes
@@ -8,14 +9,13 @@
  *
  * Description: The attributes of a dog.
  */
-
-
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
+
 /**
  * dog_t - Typedef for dog structure
  */
@@ -25,3 +25,5 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
+#endif
