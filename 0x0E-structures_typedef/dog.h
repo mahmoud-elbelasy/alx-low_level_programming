6486cfde8d0b233e4ab65a3c4b5e05356d467ec6
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-03-20 18:46:11 -0700

    g

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * struct dog - Dog attributes
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: The attributes of a dog.
+ */
+
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
