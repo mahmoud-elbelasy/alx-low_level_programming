eaaa5a788272d9c5ab37aace4cdd7b38399f7432
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-03-22 14:22:19 -0700

    update

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,11 +6,11 @@
  * @f: pointer to function
  * Return: nothing
  **/
-	void print_name(char *name, void (*f)(char *))
-	{
-		if (name == NULL || f == NULL)
-			return;
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
 
 
-		f(name);
-	}
+	f(name);
+}
