03225d03b3e3b557f5964dab609fc032599b58a1
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-03-22 13:16:09 -0700

    pushing task pointer function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: the pointer
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+	printf("%s", name);
+}
