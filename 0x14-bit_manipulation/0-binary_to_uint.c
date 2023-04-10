1458b4f8ee0f4224add745fb5eea7f65204c17ee
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-04-10 07:27:19 -0700

    converting to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * binary_to_uint - convert binary to decimal
+ * @b: the binary number
+ * Return the decimal value
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int decimal;
+
+	decimal = 0;
+
+	if (!b)
+		return (0);
+
+	for (; *b != '\0' ; b++)
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		decimal = decimal << 1;
+
+		if (*b == '1')
+			decimal = decimal | 1;
+	}
+	return (decimal);
+}
+
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+this my personal work in ALX
